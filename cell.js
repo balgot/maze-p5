b916f95fe6649088a2abb5fc439bb48fa3e0e555
Michal Barnisin <xbarnis@fi.muni.cz>
2020-09-29 00:18:17 +0200

    Add generating, moving start and target

--- cell.js
+++ cell.js
@@ -1,3 +1,6 @@
+const LINE_WIDTH = .5;
+const CELL_SIZE = SIZE - LINE_WIDTH;
+
 
 
 /* Represents a cell on the maze. TODO: keeps track of the allowed paths,
@@ -6,21 +9,51 @@ class Cell {
     constructor(row, col) {
         this.row = row;
         this.col = col;
+        // Top, Left wall
+        this.walls = undefined;
+        this.x = undefined;
+        this.y = undefined;
+        this.reset();
     }
 
     /* Find coordinates of the edge of the cell */
     edge_coordinates() {
         return {
-            x: this.row * SIZE,
-            y: this.col * SIZE,
+            y: this.row * SIZE,
+            x: this.col * SIZE,
         };
     }
 
-    show() {
-        let edge = this.edge_coordinates();
+    show(clr=color(255, 255, 255, 40)) {
+        // Draw borders
+        stroke(51, 51, 51, 40);
+        strokeWeight(LINE_WIDTH);
+        if (this.walls[0])
+            line(this.x, this.y, this.x + SIZE, this.y);
+        if (this.walls[1])
+            line(this.x, this.y, this.x, this.y + SIZE);
 
         // Fill with white where the cell is, leaving 1px for border
-        fill(255, 255, 255);
-        rect(edge.x, edge.y, SIZE - 1, SIZE - 1);
+        noStroke();
+        fill(clr);
+        rect(this.x + LINE_WIDTH*this.walls[0], 
+            this.y + LINE_WIDTH*this.walls[1], 
+            CELL_SIZE, CELL_SIZE);
+    }
+
+    /* Draws little cirlce within the cell of specified color @clr. */
+    highlight(clr) {
+        noStroke();
+        fill(clr);
+        for (let r = 0; r < SIZE; r += SIZE/5)
+            ellipse(this.x + SIZE/2, this.y + SIZE/2, r);
+    }
+
+    /* Turns the cell to initial state */
+    reset() {
+        this.walls = [true, true];
+        let edge = this.edge_coordinates();
+        this.x = edge.x;
+        this.y = edge.y;
     }
 }
--- sketch.js
+++ sketch.js
@@ -8,6 +8,19 @@ const SIZE = 40;
 let cols = 0;
 let rows = 0;
 
+/* Start of the maze, end of the maze */
+let start = undefined;
+let end = undefined;
+
+/* Current cell during exploring... */
+let current = undefined;
+
+/* Booleans of current operations */
+let generating = false;
+
+/* Any generator used during functions */
+let generator = undefined;
+
 
 
 /* Prepare the environment, create buttons, sliders and canvas */
@@ -15,12 +28,15 @@ function setup() {
     // Create canvas fullscreen TODO: add 
     let canvas = createCanvas(640, 480);
 
+    // Set white background
+    background(255);
+
     // Position the canvas within the correct parent element
     canvas.parent("content");
 
     // Calculate dimensions
-    cols = int(height / SIZE);
-    rows = int(width / SIZE);
+    rows = int(height / SIZE);
+    cols = int(width / SIZE);
 
     // Create the grid
     for (let i = 0; i < rows; ++i) {
@@ -28,16 +44,118 @@ function setup() {
         for (let j = 0; j < cols; ++j)
             grid[i].push(new Cell(i, j));
     }
+
+    // Set starting and ending point
+    start = grid[0][0];
+    end = grid[rows - 1][cols - 1];
+
+    // Set framerate to lower
+    frameRate(7);
+}
+
+
+/* Logic of the app. Called once per draw(). */
+function logic() {
+    if (generating && generator)
+        generator.next();
 }
 
 
 /* Draw all the elements, TODO: add logic here */
 function draw() {
-    // Set dark background
-    background(51);
+    // Run functions
+    logic();
 
     // Display all the cells
     for (let i = 0; i < rows; ++i)
         for (let j = 0; j < cols; ++j)
             grid[i][j].show();
+
+    // Set colors to start, end and current
+    if (start)
+        start.highlight(color(5, 153, 7, 20));
+    if (end)
+        end.highlight(color(168, 17, 12, 20));
+    if (current)
+        current.show(color(4, 176, 153, 255));
 }
+
+
+/* Returns grid to original state */
+function clear_maze() {
+    console.log("Clearing...");
+    current = undefined;
+    generator = undefined;
+    for (let row of grid)
+        for (let cell of row)
+            cell.reset();
+}
+
+
+/*************************************************
+/*  Functions with moving **start** and **end** 
+/*************************************************/
+
+/* Cell being dragged */
+let dragged_cell = undefined;
+
+/* Offset from mouse coords to left top corner */
+let offset_x = undefined;
+let offset_y = undefined;
+
+
+/* Return true if mouse is over given @cell */
+function is_mouse_over(cell) {
+    return cell.x <= mouseX && mouseX <= cell.x + SIZE
+        && cell.y <= mouseY && mouseY <= cell.y + SIZE;
+}
+
+
+function mousePressed() {
+    let draggable = [start, end];
+    for (let cell of draggable) 
+        if (cell && is_mouse_over(cell)) {
+            dragged_cell = cell;
+            grid[cell.row][cell.col] = new Cell(cell.row, cell.col);
+            offset_x = dragged_cell.x - mouseX;
+            offset_y = dragged_cell.y - mouseY;
+            console.log("Dragging [row, col] = " + [cell.row, cell.col])
+            return;
+        }
+}
+
+
+function mouseDragged() {
+    if (dragged_cell) {
+        dragged_cell.x = mouseX + offset_x;
+        dragged_cell.y = mouseY + offset_y;
+    }
+}
+
+
+function mouseReleased() {
+    if (!dragged_cell)
+        return;
+
+    // TODO: does not work
+    if (mouseX < 0 || mouseY < 0 || mouseX > width || mouseY > height) {
+        console.log("Mouse left outside grid, reset position of dragged cell to"
+            + dragged_cell.row + " - " + dragged_cell.col);
+        let row = dragged_cell.row;
+        let col = dragged_cell.col;
+        grid[row][col] = dragged_cell;
+        console.log(grid[row][col].color + "--" + dragged_cell.color);
+    }
+    else {        
+        let col = int(mouseX / SIZE);
+        let row = int(mouseY / SIZE);
+        dragged_cell.x = grid[row][col].x;
+        dragged_cell.y = grid[row][col].y;
+        dragged_cell.row = row;
+        dragged_cell.col = col;
+        grid[row][col] = dragged_cell;
+    }
+
+    dragged_cell = undefined;
+}
+
